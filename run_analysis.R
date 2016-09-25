a359ad8cb0af6bc8155f481719f6246a3943a591
Daniel A Marshall <danmarshall1983@gmail.com>
2016-09-25 14:21:02 -0400

    Clean up wrapper function and rename to runAnalysis

--- run_analysis.R
+++ run_analysis.R
@@ -1,87 +1,47 @@
+##This collection of function will download the UCI HAR Dataset process the 
+##data into a complete and tidy dataset. Then it will summarize the dataset by
+##subject, activity, and mean of selected measures. This new dataset will get
+##written to uci-har-means-tidy.csv in the working directory. The runAnalysis
+##is a wrapper and will run all needed support functions.
 
-main <- function(){
+runAnalysis <- function(){
     # Prepare Libraries
     getLibraries()
+    
     # Get the data
     getData()
+    
     # Combine data into a unified dataset
+    writeLines("Combining the data.")
+    combinedDataset <- createCombinedDataset()
     
+    #Create the Tidy summary dataset
+    writeLines("Summarizing the data.")
+    tidySummaryData <- createSummaryDataset(combinedDataset)
     
-    xtrain <- read.table("./UCI HAR Dataset/train/x_train.txt")
-    ytrain <- read.table("./UCI HAR Dataset/train/y_train.txt")
-    subtrain <- read.table("./UCI HAR Dataset/train/subject_train.txt")
-    subtest <- read.table("./UCI HAR Dataset/test/subject_test.txt")
-    features <- read.table("./UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)
-    xtest <- read.table("./UCI HAR Dataset/test/X_test.txt")
-    ytest <- read.table("./UCI HAR Dataset/test/y_test.txt")
-    activitylabels <- read.table("./UCI HAR Dataset/activity_labels.txt")
-
-    #make the names unique
-    featurelabels <- make.unique(features$V2)
+    #write the file to uci-har-means-tidy.txt
+    write.csv(summary, "uci-har-means-tidy.csv", row.names = FALSE)
+    writeLines(paste0("Tidy summary file has been saved to ", getwd(), "/uci-har-means-tidy.csv"))
+    writeLines("To view the file run: \ndata <- read.csv('uci-har-means-tidy.csv')\nView(data)")
     
-    
-    
-    #add the column labels
-    names(xtrain) <- featurelabels
-    names(xtest) <- featurelabels
-    
-    #cbind the xy for each
-    xytrain <- cbind(activity = ytrain$V1, xtrain)
-    xytest <- cbind(activity = ytest$V1, xtest)
-    
-    #cbind the subject
-    xytrain <- cbind(subject = subtrain$V1, xytrain)
-    xytest <- cbind(subject = subtest$V1, xytest)
-    
-    #add the data tables together
-    xycombined <- rbind(xytrain, xytest)
-    
-    #change the levels of activityif to be the activity name
-    xycombined$activity <- as.factor(xycombined$activity)
-    
-    #make the activity name lowercase and remove the underscore
-    activities <- gsub("_", "", tolower(activitylabels$V2))
-    
-    levels(xycombined$activity) <- activities
-    #old way
-    #add the activity labels
-    #names(activitylabels) <- c("activityid", "activity")
-    #xycombined <- merge(activitylabels, xycombined, by.x = "activityid", by.y = "activityid")
-    
-    #select only the mean and std columns
-    xycombinedsubset <- select(xycombined, subject, activity, grep("[Mm]ean|[Ss]td", names(xycombined)))
-    
-    #create the mean file
-    summarydf <- xycombinedsubset %>% group_by(subject, activity) %>% summarise_each(funs(mean))
-    
-    #make tidy names
-    tidynames <- names(summarydf)
-    tidynames <- gsub("-mean", "Mean",tidynames)
-    tidynames <- gsub("-std", "Std",tidynames)
-    tidynames <- gsub("\\(\\)-|\\(\\)", "",tidynames)
-    tidynames <- gsub("BodyBody", "Body",tidynames)
-    
-    names(summarydf) <- tidynames
-    
-    return(summarydf)
 }
 createSummaryDataset <- function(dataset){
     #select only the mean and std columns
-    xycombinedsubset <- select(xycombined, subject, activity, grep("[Mm]ean|[Ss]td", names(xycombined)))
+    subset <- select(dataset, subject, activity, grep("[Mm]ean|[Ss]td", names(dataset)))
     
     #create the mean file
-    summarydf <- xycombinedsubset %>% group_by(subject, activity) %>% summarise_each(funs(mean))
+    summary <- subset %>% group_by(subject, activity) %>% summarise_each(funs(mean))
     
     #make tidy names
-    tidynames <- names(summarydf)
+    tidynames <- names(summary)
     tidynames <- gsub("-mean", "Mean",tidynames)
     tidynames <- gsub("-std", "Std",tidynames)
     tidynames <- gsub("\\(\\)-|\\(\\)", "",tidynames)
     tidynames <- gsub("BodyBody", "Body",tidynames)
     
-    names(summarydf) <- tidynames
+    names(summary) <- tidynames
     
-    return(summarydf)
+    return(summary)
 }
 
 createCombinedDataset <- function(){
@@ -130,24 +90,24 @@ createCombinedDataset <- function(){
 getData <- function(){
     #check to see if the file already exists and exit download
     if(file.exists("UCI HAR Dataset")){
-        print("UCI HAR Dataset located in working directory.")
+        writeLines("UCI HAR Dataset located in working directory.")
     }else{
         download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "dataset.zip")
         unzip("dataset.zip")
         file.remove("dataset.zip")
-        print(paste0("UCI HAR Dataset downloaded and saved to ",  getwd(), "/UCI HAR Dataset"))
+        writeLines(paste0("UCI HAR Dataset downloaded and saved to ",  getwd(), "/UCI HAR Dataset"))
     }
 }
 
 getLibraries <- function(){
     #Checks for the required dplyr library and installs it if necessary
     if(require("dplyr")){
-        print("dplyr is loaded")
+        writeLines("dplyr is loaded")
     } else {
-        print("attempting to install dplyr")
+        writeLines("attempting to install dplyr")
         install.packages("dplyr")
         if(require(dplyr)){
-            print("dplyr was installed and loaded correctly")
+            writeLines("dplyr was installed and loaded correctly")
         } else {
             stop("could not install required packages (dplyr)")
         }
