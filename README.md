2171ac5d6801fd1291d74cd060ecdf5b9c87e2f5
Daniel A Marshall <danmarshall1983@gmail.com>
2016-09-25 10:08:28 -0400

    Move combined dataset creation to createCombinedDataset function

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
-#EXAMPLE PLACE HOLDER THIS MUST BE UPDATED
+#EXAMPLE PLACE HOLDER THIS MUST BE UPDATED BLAH
 
 # Getting and Cleaning Data - Course Project
+The R script `run_analysis.R` was created to gather data, clean data, and created a tidy summary dataset of the UCI HAR Dataset. `run_analysis.R` contians the following functions:
 
-This is the course project for the Getting and Cleaning Data Coursera course.
-The R script, `run_analysis.R`, does the following:
+* ###main 
+This function is a wrapper to run all necessary functions start to finish. 
 
-1. Download the dataset if it does not already exist in the working directory
-2. Load the activity and feature info
-3. Loads both the training and test datasets, keeping only those columns which
+* ###getLibraries
+This script requires the dplyr library. This function will check for this library and download and/or load if necessary.
+
+* ###getData
+This function does checks the working directory for the `UCI HAR Dataset`. If the folder does not exist the data is downloaded, unziped, and the zipped file is removed. The unziped folder location is reported to the user.
+
+
    reflect a mean or standard deviation
 4. Loads the activity and subject data for each dataset, and merges those
    columns with the dataset
--- run_analysis.R
+++ run_analysis.R
@@ -1,37 +1,10 @@
-getData <- function(){
-    #check to see if the file already exists and exit download
-    if(file.exists("UCI HAR Dataset")){
-        print("UCI HAR Dataset located in working directory.")
-    }else{
-        download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "dataset.zip")
-        unzip("dataset.zip")
-        file.remove("dataset.zip")
-        print(paste0("UCI HAR Dataset downloaded and saved to ",  getwd(), "/UCI HAR Dataset"))
-    }
-}
 
-lookForFile <- function(){
-    if(file.exists("UCI HAR Dataset")){
-        print("UIC HAR Dataset located in working directory.")
-    }
-}
-
-mainfunciton <- function(){
-    
+main <- function(){
     # Prepare Libraries
-    
-    if(require("dplyr")){
-        print("dplyr is loaded")
-    } else {
-        print("attempting to install dplyr")
-        install.packages("dplyr")
-        if(require(dplyr)){
-            print("dplyr was installed and loaded correctly")
-        } else {
-            stop("could not install required packages (dplyr)")
-        }
-    }
-    
+    getLibraries()
+    # Get the data
+    getData()
+    # Combine data into a unified dataset
     
     
     xtrain <- read.table("./UCI HAR Dataset/train/x_train.txt")
@@ -91,4 +64,92 @@ mainfunciton <- function(){
     names(summarydf) <- tidynames
     
     return(summarydf)
+}
+createSummaryDataset <- function(dataset){
+    #select only the mean and std columns
+    xycombinedsubset <- select(xycombined, subject, activity, grep("[Mm]ean|[Ss]td", names(xycombined)))
+    
+    #create the mean file
+    summarydf <- xycombinedsubset %>% group_by(subject, activity) %>% summarise_each(funs(mean))
+    
+    #make tidy names
+    tidynames <- names(summarydf)
+    tidynames <- gsub("-mean", "Mean",tidynames)
+    tidynames <- gsub("-std", "Std",tidynames)
+    tidynames <- gsub("\\(\\)-|\\(\\)", "",tidynames)
+    tidynames <- gsub("BodyBody", "Body",tidynames)
+    
+    names(summarydf) <- tidynames
+    
+    return(summarydf)
+}
+
+createCombinedDataset <- function(){
+    #Combine the training and test dataset applying the appropriate labels and headers
+    
+    #read in all necessary files
+    xtrain <- read.table("./UCI HAR Dataset/train/x_train.txt")
+    ytrain <- read.table("./UCI HAR Dataset/train/y_train.txt")
+    subtrain <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+    subtest <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+    features <- read.table("./UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)
+    xtest <- read.table("./UCI HAR Dataset/test/X_test.txt")
+    ytest <- read.table("./UCI HAR Dataset/test/y_test.txt")
+    activitylabels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+    
+    #make the names unique
+    featurelabels <- make.unique(features$V2)
+    
+    #add the column labels
+    names(xtrain) <- featurelabels
+    names(xtest) <- featurelabels
+    
+    #cbind the xy for each
+    xytrain <- cbind(activity = ytrain$V1, xtrain)
+    xytest <- cbind(activity = ytest$V1, xtest)
+    
+    #cbind the subject
+    xytrain <- cbind(subject = subtrain$V1, xytrain)
+    xytest <- cbind(subject = subtest$V1, xytest)
+    
+    #add the data tables together
+    xycombined <- rbind(xytrain, xytest)
+    
+    #change the levels of activityif to be the activity name
+    xycombined$activity <- as.factor(xycombined$activity)
+    
+    #make the activity name lowercase and remove the underscore
+    activities <- gsub("_", "", tolower(activitylabels$V2))
+    levels(xycombined$activity) <- activities
+    
+    return(xycombined)
+}
+
+
+
+getData <- function(){
+    #check to see if the file already exists and exit download
+    if(file.exists("UCI HAR Dataset")){
+        print("UCI HAR Dataset located in working directory.")
+    }else{
+        download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "dataset.zip")
+        unzip("dataset.zip")
+        file.remove("dataset.zip")
+        print(paste0("UCI HAR Dataset downloaded and saved to ",  getwd(), "/UCI HAR Dataset"))
+    }
+}
+
+getLibraries <- function(){
+    #Checks for the required dplyr library and installs it if necessary
+    if(require("dplyr")){
+        print("dplyr is loaded")
+    } else {
+        print("attempting to install dplyr")
+        install.packages("dplyr")
+        if(require(dplyr)){
+            print("dplyr was installed and loaded correctly")
+        } else {
+            stop("could not install required packages (dplyr)")
+        }
+    }
 }
